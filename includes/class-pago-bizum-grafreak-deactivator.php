220b1609ebf21f08400bcf4896cc304aea352f72
one1973 <one1973@hotmail.com>
2026-03-19 18:26:31 +0100

    Add files via upload

---
+++ includes/class-pago-bizum-grafreak-deactivator.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.grafreak.net
+ * @since      1.0.0
+ *
+ * @package    Pago_Bizum_Grafreak
+ * @subpackage Pago_Bizum_Grafreak/includes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pago_Bizum_Grafreak
+ * @subpackage Pago_Bizum_Grafreak/includes
+ * @author     Adrian Cobo <adrian@grafreak.net>
+ */
+class Pago_Bizum_Grafreak_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
